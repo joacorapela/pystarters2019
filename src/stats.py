74ef48c5c7c92e0442a06310815c5419af84f0d1
Joaquin Rapela <joacorapela@gmail.com>
2019-11-24 02:13:15 +0000

    Added files

---
+++ src/stats.py
@@ -0,0 +1,28 @@
+
+import numpy as np
+import statsmodels.api as sm
+import bootstrapped.bootstrap as bs
+import ipdb
+
+def getCoefsAndPValues(x, y):
+    regressors = sm.add_constant(x)
+    fit = sm.OLS(y, regressors).fit()
+    return fit.params, fit.pvalues
+
+def getCoefsAndSlopeCI(x, y, nIterations=30000):
+
+    def getSlope(data):
+        x = data[:,0]
+        y = data[:,1]
+        regressors = sm.add_constant(x)
+        fit = sm.OLS(y, regressors).fit()
+        return [fit.params[1]]
+
+    regressors = sm.add_constant(x)
+    fit = sm.OLS(y, regressors).fit()
+    coefs = fit.params
+
+    bootData = np.column_stack((x,y))
+    bootRes = bs.bootstrap(bootData, stat_func=getSlope, num_iterations=1000)
+    return coefs, (bootRes.lower_bound, bootRes.upper_bound)
+
